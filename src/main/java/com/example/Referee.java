967195cf1a9f1843803aaae7da71f228513fde89
Masatoshi Tada <rosso.e.nero84@gmail.com>
2021-05-14 14:02:02 +0900

    不要なメソッドを削除

--- src/main/java/com/example/Referee.java
+++ src/main/java/com/example/Referee.java
@@ -16,18 +16,12 @@ public class Referee {
             return Result.PLAYER_WIN;
         }
         // 21点との差を計算して勝敗を返す
-        int playerDiff = calculateDiff(playerTotal);
-        int dealerDiff = calculateDiff(dealerTotal);
-        if (playerDiff == dealerDiff) {
+        if (playerTotal == dealerTotal) {
             return Result.DRAW;
-        } else if (playerDiff < dealerDiff) {
+        } else if (playerTotal > dealerTotal) {
             return Result.PLAYER_WIN;
         } else {
             return Result.DEALER_WIN;
         }
-    }
-
-    private int calculateDiff(int total) {
-        return Hands.BUST_IF_TOTAL_IS_OVER - total;
     }
 }
