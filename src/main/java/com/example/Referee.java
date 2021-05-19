8f22db5d964d9359e32ff80b778faff7a094570f
Masatoshi Tada <rosso.e.nero84@gmail.com>
2021-05-19 16:50:44 +0900

    勝敗判定ロジックの冗長さを修正

--- src/main/java/com/example/Referee.java
+++ src/main/java/com/example/Referee.java
@@ -3,17 +3,15 @@ package com.example;
 public class Referee {
 
     public Result judge(Player player, Dealer dealer) {
-        int playerTotal = player.total();
-        int dealerTotal = dealer.total();
-        boolean playerIsBust = player.isBust();
-        boolean dealerIsBust = dealer.isBust();
         // バーストかどうかを判断して勝敗を返す
-        if (playerIsBust) {
+        if (player.isBust()) {
             return Result.DEALER_WIN;
-        } else if (dealerIsBust) {
+        } else if (dealer.isBust()) {
             return Result.PLAYER_WIN;
         }
         // 得点を比較して勝敗を返す
+        int playerTotal = player.total();
+        int dealerTotal = dealer.total();
         if (playerTotal == dealerTotal) {
             return Result.DRAW;
         } else if (playerTotal > dealerTotal) {
